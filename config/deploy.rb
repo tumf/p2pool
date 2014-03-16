4bfef1b7845638206b55d11d4e139bca0a2a6540
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2014-03-16 21:58:39 +0900

    set repos

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'p2pool'
+set :repo_url, 'https://github.com/forrestv/p2pool.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/p2pool/app'
 
 # Default value for :scm is :git
 # set :scm, :git
